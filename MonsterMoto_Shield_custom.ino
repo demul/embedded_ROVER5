3a82c750b655f8c6bafc9034fb6bf2fa1b7dd332
JEONG HYEONJIN <38728285+demul@users.noreply.github.com>
2020-08-30 21:49:29 +0900

    Update Firmware
    Add Protocol Macro

--- MonsterMoto_Shield_custom.ino
+++ MonsterMoto_Shield_custom.ino
@@ -1,37 +1,12 @@
-/*  MonsterMoto Shield Example Sketch
-  date: 5/24/11
-  code by: Jim Lindblom
-  hardware by: Nate Bernstein
-  SparkFun Electronics
- 
- License: CC-SA 3.0, feel free to use this code however you'd like.
- Please improve upon it! Let me know how you've made it better.
- 
- This is really simple example code to get you some basic
- functionality with the MonsterMoto Shield. The MonsterMote uses
- two VNH2SP30 high-current full-bridge motor drivers.
- 
- Use the motorGo(uint8_t motor, uint8_t direct, uint8_t pwm) 
- function to get motors going in either CW, CCW, BRAKEVCC, or 
- BRAKEGND. Use motorOff(int motor) to turn a specific motor off.
- 
- The motor variable in each function should be either a 0 or a 1.
- pwm in the motorGo function should be a value between 0 and 255.
- */
 #define BRAKEVCC 0
 #define CW   1
 #define CCW  2
 #define BRAKEGND 3
 #define CS_THRESHOLD 100
 
-struct charcharintint
-{
-  
-}
-
-/*  VNH2SP30 pin definitions
- xxx[0] controls '1' outputs
- xxx[1] controls '2' outputs */
+#define DECODE(encoding, pwm, cw) cw = (encoding & (char)1) + 1;pwm = (encoding>>1)<<3;
+                                  
+                                  
 int inApin[2] = {7, 4};  // INA: Clockwise input
 int inBpin[2] = {8, 9}; // INB: Counter-clockwise input
 int pwmpin[2] = {5, 6}; // PWM input
@@ -39,6 +14,7 @@ int cspin[2] = {2, 3}; // CS: Current sense ANALOG input
 int enpin[2] = {0, 1}; // EN: Status of switches output (Analog pin)
 
 int statpin = 13;
+
 
 void setup()
 {
@@ -59,14 +35,16 @@ void setup()
     digitalWrite(inApin[i], LOW);
     digitalWrite(inBpin[i], LOW);
   }
-  // motorGo(0, CW, 1023);
-  // motorGo(1, CCW, 1023);
+
 }
 
 char temp;
+char encoding[2];
+int pwm;
+char cw;
+
 void loop()
 {
-  
   while(Serial.available() <=0) // wait for incoming serial data
   {
   }
@@ -103,18 +81,16 @@ void loop()
               motorOff(0);
               motorOff(1);
               break;  
-
+         
+        case 'k':
+              Serial.println("k");
+              Serial.readBytes(encoding, 2);
+              DECODE(encoding[0], pwm, cw);
+              motorGo(0, cw, pwm);
+              DECODE(encoding[1], pwm, cw);
+              motorGo(1, cw, pwm);
+              break;
   }
-//  motorGo(0, CW, 1023);
-//  motorGo(1, CCW, 1023);
-//  delay(500);
-
-//  motorGo(0, CCW, 1023);
-//  motorGo(1, CW, 1023);
-//  delay(500);
-  
-//  if ((analogRead(cspin[0]) < CS_THRESHOLD) && (analogRead(cspin[1]) < CS_THRESHOLD))
-//    digitalWrite(statpin, HIGH);
 }
 
 void motorOff(int motor)
@@ -128,22 +104,6 @@ void motorOff(int motor)
   analogWrite(pwmpin[motor], 0);
 }
 
-/* motorGo() will set a motor going in a specific direction
- the motor will continue going in that direction, at that speed
- until told to do otherwise.
- 
- motor: this should be either 0 or 1, will selet which of the two
- motors to be controlled
- 
- direct: Should be between 0 and 3, with the following result
- 0: Brake to VCC
- 1: Clockwise
- 2: CounterClockwise
- 3: Brake to GND
- 
- pwm: should be a value between ? and 1023, higher the number, the faster
- it'll go
- */
 void motorGo(uint8_t motor, uint8_t direct, uint8_t pwm)
 {
   if (motor <= 1)
